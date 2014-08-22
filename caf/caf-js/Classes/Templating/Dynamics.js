834f2c98f5dc8494aebb80fb54dcb8efde7746e3
codletech <codletech@gmail.com>
2014-08-22 22:07:37 +0300

    Dynamics 1.1

--- caf/caf-js/Classes/Templating/Dynamics.js
+++ caf/caf-js/Classes/Templating/Dynamics.js
@@ -3,8 +3,18 @@
  */
 var CDynamics = Class({
     $singleton: true,
-    applyDynamic: function() {
-        
+    applyDynamic: function(object,value) {
+        if (CUtils.isEmpty(value) || CUtils.isEmpty(value.url))
+            return;
+        // Initialize and set defaults.
+        object.dynamic              = CUtils.clone(value);
+        object.dynamic.url          = object.dynamic.url        || '';
+        object.dynamic.data         = object.dynamic.data       || {};
+        object.dynamic.autoLoad     = object.dynamic.autoLoad   || false;
+        object.dynamic.loaded       = object.dynamic.loaded     || false;
+        object.dynamic.abstract     = object.dynamic.abstract   === false ? false : true;
+        object.dynamic.duplicates   = object.dynamic.data       || [];
+
     }
 
 });
